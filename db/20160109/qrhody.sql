28573c240459cce1e72983697c5a11581d759da9
kangyu <kangyu@sunline.cn>
2016-01-09 19:29:06 +0800

    1

---
+++ db/20160109/qrhody.sql
@@ -0,0 +1,5 @@
+insert into ap_sys_column (COLUMN_CD, COLUMN_NAME, COLUMN_LENGTH, COLUMN_MAPPING, COLUMN_TYPE, VALUE_DIGIT, DATE_PATTERN, DEFAULT_VALUE, CYCLING_FLAG, SORT_SEQ, CYCLING_COLUMN, POLISH_TYPE, POLISH_CHAR, MSG_CD)
+values ('remark', '节假日名称', null, 'remark', 'S', '', '', '', 'N', 6, 'input', '', '', 'qrhodyreq');
+
+commit;
+
